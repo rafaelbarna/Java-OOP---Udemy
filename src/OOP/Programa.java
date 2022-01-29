1b655627e365dc9d17acd6964bcfb07767001630
Rafael Barna <rafael@barna.com.br>
2022-01-29 20:40:29 -0300

    Adicionada Nova Instância de Produto, e Entrada de Dados do Produto

--- src/OOP/Programa.java
+++ src/OOP/Programa.java
@@ -7,5 +7,18 @@ public class Programa {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
+		Produto produto = new Produto();
+		
+		// Leitura de dados do Produto
+		System.out.println("Insira dados do Produto:");
+		System.out.print("Nome: ");
+		produto.nome = scanner.nextLine();
+		System.out.print("Preço: ");
+		produto.preco = scanner.nextDouble();
+		System.out.print("Quantidade em estoque: ");
+		produto.quantidade = scanner.nextInt();
+		
+		System.out.println("");
+		
 	}
 }
