e25e0debff9078b2846704d3228212a4561077e8
Rafael Barna <rafael@barna.com.br>
2022-01-30 14:05:09 -0300

    Encapsulamento
    Getters and Setters

--- src/OPP_Contrutores_This_Sobrecarga_Encapsulamento/Produto.java
+++ src/OPP_Contrutores_This_Sobrecarga_Encapsulamento/Produto.java
@@ -3,32 +3,47 @@ package OPP_Contrutores_This_Sobrecarga_Encapsulamento;
 public class Produto {
 
 	// Atributos
-	public String nome;
-	public double preco;
-	public int quantidade;
-	
-	// Construtor Padrão
+	private String nome;
+	private double preco;
+	private int quantidade;
+
+	// Construtor
 	public Produto() {
 	}
 
-	// Construtor ~ Geralmente colocado depois dos Atributos e antes dos Métodos
-	// ~ Executa no momento da Instânciação do Objeto
-	// public NomeDaClasse(parâmetros, parâmetros, parâmetros)
 	public Produto(String nome, double preco, int quantidade) {
-		// Corpo do Contrutor
-		// this. ~ Referencia ao Atributo do próprio Objeto
-		// = variavel ~ Referencia o Parâmetro
 		this.nome = nome;
 		this.preco = preco;
 		this.quantidade = quantidade;
 	}
-	
-	// Sobrecarga de Construtor
+
+	// Sobrecarga do Construtor
 	public Produto(String nome, double preco) {
 		this.nome = nome;
 		this.preco = preco;
 	}
-	
+
+	// Getters and Setters
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
 	// Métodos
 	public double totalEstoque() {
 		return preco * quantidade;
@@ -41,10 +56,11 @@ public class Produto {
 	public void removeProduto(int quantidade) {
 		this.quantidade -= quantidade;
 	}
-	
+
 	// Implementa versão customizada do toString()
 	public String toString() {
 		// %.2f ~ Define o número de casas decimais
-		return "Nome: " + nome + ", Preco: R$" + String.format("%.2f", preco) + ". Quantidade: " + quantidade + ". Valor total em estoque: R$" + String.format("%.2f", totalEstoque());
+		return "\nNome: " + nome + ", Preco: R$" + String.format("%.2f", preco) + ". Quantidade: " + quantidade
+				+ ". Valor total em estoque: R$" + String.format("%.2f", totalEstoque());
 	}
 }
--- src/OPP_Contrutores_This_Sobrecarga_Encapsulamento/Programa.java
+++ src/OPP_Contrutores_This_Sobrecarga_Encapsulamento/Programa.java
@@ -3,14 +3,11 @@ package OPP_Contrutores_This_Sobrecarga_Encapsulamento;
 import java.util.Scanner;
 
 public class Programa {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
-		
-		// Construtor Padrão
-		// Produto produto = new Produto();
-		
+
 		// Leitura de dados do Produto
 		System.out.println("Insira dados do Produto:");
 		System.out.print("Nome: ");
@@ -19,32 +16,28 @@ public class Programa {
 		double preco = scanner.nextDouble();
 		System.out.print("Quantidade em estoque: ");
 		int quantidade = scanner.nextInt();
-		
+
 		// Construtor com Parâmetros
 		Produto produto = new Produto(nome, preco, quantidade);
-		
-		// Construtor com Sobrecarga
-		// Produto produto = new Produto(nome, preco);
-		
-		// Retorna Produto cadastrado ~ Como String já formatada utilizando o método toString "modificado"
 		System.out.println(produto);
 		
+		produto.setNome("TV 8k");
+		System.out.println("Nome atualizado: " + produto.getNome());
+		produto.setPreco(9000);
+		System.out.println("Preço atualizado: " + produto.getPreco());
+
 		// Adiciona Produto no estoque
 		System.out.print("\nQuantos Produtos deseja adicionar em Estoque:");
 		quantidade = scanner.nextInt();
-		// Atualiza o Objeto Produto com a Quantidade de itens
 		produto.adicionaProduto(quantidade);
 		System.out.println(produto);
-		
+
 		// Remove Produto do estoque
 		System.out.println("\nQuantos Produtos deseja remover do Estqoue:");
-		// Reutilizando a Variável quantidade
 		quantidade = scanner.nextInt();
-		// Utiliza a Função removeProduto passando como parametro quantidade
 		produto.removeProduto(quantidade);
 		System.out.println(produto);
-		
-		// "Fecha" a Função Scanner
+
 		scanner.close();
 	}
 }
