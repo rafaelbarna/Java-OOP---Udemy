151cefd8e9dfe72d1d753308308c1452c9b33f42
Rafael Barna <rafael@barna.com.br>
2022-01-29 21:11:23 -0300

    Implementada funções de Adicionar e Remover Produtos

--- src/OOP/Programa.java
+++ src/OOP/Programa.java
@@ -18,7 +18,25 @@ public class Programa {
 		System.out.print("Quantidade em estoque: ");
 		produto.quantidade = scanner.nextInt();
 		
+		// Retorna Produto cadastrado ~ Como String já formatada utilizando o método toString "modificado"
 		System.out.println(produto);
 		
+		// Adiciona Produto no estoque
+		System.out.print("\nQuantos Produtos deseja adicionar em Estoque:");
+		int quantidade = scanner.nextInt();
+		// Atualiza o Objeto Produto com a Quantidade de itens
+		produto.adicionaProduto(quantidade);
+		System.out.println(produto);
+		
+		// Remove Produto do estoque
+		System.out.println("Quantos Produtos deseja remover do Estqoue:");
+		// Reutilizando a Variável quantidade
+		quantidade = scanner.nextInt();
+		// Utiliza a Função removeProduto passando como parametro quantidade
+		produto.removeProduto(quantidade);
+		System.out.println(produto);
+		
+		// "Fecha" a Função Scanner
+		scanner.close();
 	}
 }
